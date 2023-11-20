e3058f1f507f127361ddaae669a415a1f47de812
maverickamey <ameyjuvatkar123@gmail.com>
2023-11-20 02:27:21 -0800

    Removed unwanted

--- LibraryManagementSystem/LibraryManagementSystem/src/main/java/com/example/LibraryManagementSystem/models/Genre.java
+++
@@ -1,9 +0,0 @@
-package com.example.LibraryManagementSystem.models;
-
-// Enum are stored as integer so based on value it is stored as String
-public enum Genre {
-
-    FICTION,
-    PROGRAMMING,
-    STORY
-}
--- jpaLearning/jpaLearning/src/main/java/com/LearningJPA/jpaLearning/dao/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.LearningJPA.jpaLearning.dao;
-
-import com.LearningJPA.jpaLearning.models.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin,Integer> {
-
-}
